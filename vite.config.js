ce5f8f64c8e4e1b9b4182f98926c9dfb03facf05
JacobBuell03 <2003jtb@gmail.com>
2026-07-05 22:54:52 -0400

    Fix base path case

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Techfolio/',
+  base: '/TechFolio/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
